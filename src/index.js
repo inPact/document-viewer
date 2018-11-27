eb53aa03c171f3a3ce4a0dbc1fc89a748606cd8a
shir brunner <shir@tabit.cloud>
2018-11-27 16:43:15 +0200

    conditional export

--- src/index.js
+++ src/index.js
@@ -1,27 +1,28 @@
 import './css/tlogDocsTemplate.scss';
 import TlogDocsService from './tlog-docs-template/tlogDocsService';
-window.DocumentViewer = class DocumentViewer {
 
-  constructor(options = {}) {
-    this.options = {
-      isUS: false,
-      local: 'he-IL'
+class DocumentViewer {
+    constructor() {
+        this._tlogDocsService = new TlogDocsService({
+            isUS: false,
+            local: 'he-IL'
+        });
     }
-    this._tlogDocsService = new TlogDocsService(this.options);
-  }
 
+    getDocumentsInfoFromTlog(tlog, printData) {
+        return this._tlogDocsService.getDocs(tlog, printData);
+    }
 
+    getHTMLDocument(documentInfo, printData) {
+        return this._tlogDocsService.getHTMLDocument(documentInfo, printData);
+    }
 
-  getDocumentsInfoFromTlog(tlog , printData) {
-    return this._tlogDocsService.getDocs(tlog,printData);
-  }
+    getHTMLDocumentWithoutTlog(document, options) {
+        return this._tlogDocsService.getHTMLDocumentWithoutTlog(document, options);
+    }
+}
 
-  getHTMLDocument(documentInfo, printData) {
-    return this._tlogDocsService.getHTMLDocument(documentInfo, printData);
-  }
-
-  getHTMLDocumentWithoutTlog(document, options) {
-    return this._tlogDocsService.getHTMLDocumentWithoutTlog(document, options);
-  }
-};
-
+if(window)
+    window.DocumentViewer = DocumentViewer;
+else
+    module.exports = DocumentViewer;
