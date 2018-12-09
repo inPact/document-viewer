1fb86d272883f0e2caf28246f78b6f9ae3756576
ilanz <ilan@tabit.cloud>
2018-12-09 13:55:48 +0200

    fixed offer Units for item with weight

--- src/tlog-docs-template/templateBuilderService.js
+++ src/tlog-docs-template/templateBuilderService.js
@@ -442,8 +442,14 @@ export default class TemplateBuilderService {
                 if (!(data.isReturnOrder && this._docData.documentType === 'orderBill' && (!item.amount || item.amount === '0.00'))) {
 
                     var orderdOfferListExists = printData.collections.ORDERED_OFFERS_LIST.length > 0 ? true : false;
-                    console.log(printData);
-                    var offerUnits = orderdOfferListExists && printData.collections.ORDERED_OFFERS_LIST[index].OFFER_UNITS ? printData.collections.ORDERED_OFFERS_LIST[index].OFFER_UNITS : null;
+                    var offerUnits;
+                    if (orderdOfferListExists) {
+                        printData.collections.ORDERED_OFFERS_LIST.forEach((order) => {
+                            order.OFFER_UNITS ? offerUnits = order.OFFER_UNITS : offerUnits = null
+                        })
+
+                    }
+
                     var isWeightItem = offerUnits && offerUnits > 0 ? true : false;
                     var weightWordTranslate = this.$translate.getText('weight');
 
