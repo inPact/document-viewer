afb10f17f2b8a507fa88dc4357fa86207409a376
ilanz <ilan@tabit.cloud>
2018-12-23 18:18:53 +0200

    added signature field

--- src/tlog-docs-template/templateBuilderService.js
+++ src/tlog-docs-template/templateBuilderService.js
@@ -470,10 +470,51 @@ export default class TemplateBuilderService {
                 var creditPaymentDiv = this.createCreditTemplate(printData);
                 tplOrderPaymentsDiv.appendChild(creditPaymentDiv);
 
-                if (this._docObj.md && !this._isUS) {
+                if (this._docObj.md && !this._isUS && this._docObj.docPaymentType === "CreditCardPayment") {
                     var signatureArea = this._doc.createElement('div');
                     signatureArea.id = 'signatureArea';
-                    tplOrderPaymentsDiv.appendChild(this.$signatureService.getSignature(this._docObj, signatureArea, this._doc));
+                    if (docObjChosen.md.signature) {
+
+                        let signatureData = docObjChosen.md.signature;
+                        let signatureDiv = this._doc.createElement('div');
+                        signatureDiv.id = 'signatureDiv';
+                        signatureDiv.classList += " signature-container";
+
+                        let elementSVGDiv = this._doc.createElement('div');
+                        elementSVGDiv.id = 'elementSVGDiv'
+                        elementSVGDiv.classList += " signature-container";
+                        let newSVG = this._doc.createElement('div');
+                        newSVG.id = 'newSVG';
+
+                        elementSVGDiv.appendChild(newSVG)
+                        newSVG.outerHTML += `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id='svg' width='100%' height='100px' transform='translate(0, 0)'  viewBox="0 0 500 150" ></svg>`
+                        let svgNode = elementSVGDiv.getElementsByTagName('svg')[0];
+                        svgNode.classList += " signature-container";
+
+                        elementSVGDiv.appendChild(svgNode);
+
+                        signatureDiv.appendChild(elementSVGDiv)
+
+                        let elementSVG = signatureDiv.getElementsByTagName('svg')[0];
+                        elementSVG.id = 'elementSVG';
+
+                        let path = this.$signatureService.makeSVG('path', { d: signatureData, version: "1.1", xmlns: "http://www.w3.org/2000/svg", stroke: "#06067f", 'stroke-width': "2", height: "auto", transform: 'translate(50,-40) scale(0.4,0.4)', 'stroke-linecap': "butt", fill: "none", 'stroke-linejoin': "miter" });
+                        path.setAttribute("width", "50%");
+                        path.setAttribute("height", "auto");
+
+                        elementSVG.setAttribute("width", "100");
+                        elementSVG.setAttribute("height", "auto");
+
+                        elementSVG.innerHTML = "";
+                        elementSVG.appendChild(path);
+                        elementSVG.setAttribute("width", "100%");
+                        elementSVG.setAttribute("height", "auto");
+
+                        elementSVGDiv.appendChild(elementSVG);
+                        signatureArea.appendChild(signatureDiv)
+                    }
+                    tplOrderPaymentsDiv.appendChild(signatureArea);
+                    // tplOrderPaymentsDiv.appendChild(this.$signatureService.getSignature(this._docObj, signatureArea, this._doc));
                 }
             }
             else if (this._docObj.docPaymentType === ("GiftCard")) {
