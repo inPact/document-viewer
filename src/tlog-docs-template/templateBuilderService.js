40795f9177318b1483c90186438af0a264ec0612
gal gimpel <gal@tabit.cloud>
2018-12-24 18:31:19 +0200

    update

--- src/tlog-docs-template/templateBuilderService.js
+++ src/tlog-docs-template/templateBuilderService.js
@@ -480,7 +480,7 @@ export default class TemplateBuilderService {
                 var creditPaymentDiv = this.createCreditTemplate(printData);
                 tplOrderPaymentsDiv.appendChild(creditPaymentDiv);
 
-                if (this._docObj.md.signauture && !this._isUS && this._docObj.docPaymentType === "CreditCardPayment") {
+                if (this._docObj.md.signature && !this._isUS && this._docObj.docPaymentType === "CreditCardPayment") {
                     var signatureArea = this._doc.createElement('div');
                     signatureArea.id = 'signatureArea';
                     signatureArea.className += ' item-div';
