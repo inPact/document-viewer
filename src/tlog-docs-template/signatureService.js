24278d6c023865a467e742ecc4b33758e79d7d8d
gal gimpel <gal@tabit.cloud>
2018-12-24 14:24:04 +0200

    udpate

--- src/tlog-docs-template/signatureService.js
+++ src/tlog-docs-template/signatureService.js
@@ -8,13 +8,15 @@ export default class SignatureService {
 
     getSignature(docObjChosen, element, doc) {
         this._doc = doc;
-        
-        
+
+
         let signatureData = docObjChosen.md.signature;
-        
-        
-        
-        
+        console.log(signatureData);
+
+        debugger; 
+
+
+
         function makeSVG(tag, attrs) {
             var el = document.createElementNS('http://www.w3.org/2000/svg', tag);
             for (var k in attrs)
@@ -34,58 +36,58 @@ export default class SignatureService {
         elementSVG.html("");
         elementSVG.append(path);
         elementSVG.width(widthsignatureContenier);
-        
-        
-        
-        
-        
-        
+
+
+
+
+
+
         //////////////////////////////////////////////
-        
-        
-        
-        
-        
-        
-        
-        
-        let signatureData = docObjChosen.md.signature;
-        let signatureDiv = this._doc.createElement('div');
-        signatureDiv.id = 'signatureDiv';
-        signatureDiv.classList += " signature-container";
 
-        let elementSVGDiv = this._doc.createElement('div');
-        elementSVGDiv.id = 'elementSVGDiv'
-        elementSVGDiv.classList += " signature-container";
-        let newSVG = this._doc.createElement('div');
-        newSVG.id = 'newSVG';
 
-        elementSVGDiv.appendChild(newSVG);
-        newSVG.outerHTML += `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id='svg' width='100%' height='auto' transform='translate(0, 0)'  viewBox="0 0 500 150" ></svg>`
-        let svgNode = elementSVGDiv.getElementsByTagName('svg')[0];
-        svgNode.classList += " signature-container";
 
-        elementSVGDiv.appendChild(svgNode);
 
-        signatureDiv.appendChild(elementSVGDiv)
 
-        let elementSVG = signatureDiv.getElementsByTagName('svg')[0];
-        elementSVG.id = 'elementSVG';
 
-        let path = this.makeSVG('path', { d: signatureData, version: "1.1", xmlns: "http://www.w3.org/2000/svg", stroke: "#06067f", 'stroke-width': "2", height: "auto", transform: 'translate(50,-40) scale(0.4,0.4)', 'stroke-linecap': "butt", fill: "none", 'stroke-linejoin': "miter" });
-        path.setAttribute("width", "50%");
-        path.setAttribute("height", "auto");
 
-        elementSVG.setAttribute("width", "100%");
-        elementSVG.setAttribute("height", "auto");
 
-        elementSVG.innerHTML = "";
-        elementSVG.appendChild(path);
-        elementSVG.setAttribute("width", "100%");
-        elementSVG.setAttribute("height", "auto");
+        // let signatureData = docObjChosen.md.signature;
+        // let signatureDiv = this._doc.createElement('div');
+        // signatureDiv.id = 'signatureDiv';
+        // signatureDiv.classList += " signature-container";
 
-        elementSVGDiv.appendChild(elementSVG);
-        element.appendChild(signatureDiv)
+        // let elementSVGDiv = this._doc.createElement('div');
+        // elementSVGDiv.id = 'elementSVGDiv'
+        // elementSVGDiv.classList += " signature-container";
+        // let newSVG = this._doc.createElement('div');
+        // newSVG.id = 'newSVG';
+
+        // elementSVGDiv.appendChild(newSVG);
+        // newSVG.outerHTML += `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" id='svg' width='100%' height='auto' transform='translate(0, 0)'  viewBox="0 0 500 150" ></svg>`
+        // let svgNode = elementSVGDiv.getElementsByTagName('svg')[0];
+        // svgNode.classList += " signature-container";
+
+        // elementSVGDiv.appendChild(svgNode);
+
+        // signatureDiv.appendChild(elementSVGDiv)
+
+        // let elementSVG = signatureDiv.getElementsByTagName('svg')[0];
+        // elementSVG.id = 'elementSVG';
+
+        // let path = this.makeSVG('path', { d: signatureData, version: "1.1", xmlns: "http://www.w3.org/2000/svg", stroke: "#06067f", 'stroke-width': "2", height: "auto", transform: 'translate(50,-40) scale(0.4,0.4)', 'stroke-linecap': "butt", fill: "none", 'stroke-linejoin': "miter" });
+        // path.setAttribute("width", "50%");
+        // path.setAttribute("height", "auto");
+
+        // elementSVG.setAttribute("width", "100%");
+        // elementSVG.setAttribute("height", "auto");
+
+        // elementSVG.innerHTML = "";
+        // elementSVG.appendChild(path);
+        // elementSVG.setAttribute("width", "100%");
+        // elementSVG.setAttribute("height", "auto");
+
+        // elementSVGDiv.appendChild(elementSVG);
+        // element.appendChild(signatureDiv)
 
         return element;
     }
