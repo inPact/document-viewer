01f32880b323819ef3edc4139c0afcda182da469
gal gimpel <gal@tabit.cloud>
2018-12-26 19:21:46 +0200

    update

--- src/tlog-docs-template/addTaxDataService.js
+++ src/tlog-docs-template/addTaxDataService.js
@@ -92,7 +92,7 @@ export default class {
                 let incTaxItemDiv = doc.createElement('div');
                 incTaxItemDiv.classList += 'incTaxItemDiv';
                 incTaxItemDiv.innerHTML = "<div class='itemDiv " + (incTax.amount ? 'small-chars' : "") + "'>" +
-                    "<div class='total-name'>" + (incTax.amount ? '&nbsp;' : "") + (incTax.name ? incTax.name : " ") + (incTax.amount ? "&nbsp;" + incTax.rate + "%" : '') + "</div>" + " " +
+                    "<div class='total-name'>" + (incTax.amount ? '&nbsp;' : "") + (incTax.name ? incTax.name : " ") + "</div>" + " " +
                     "<div class='total-amount " + this.isNegative(incTax.amount) + "'> &nbsp;" + (incTax.amount ? Number(incTax.amount).toFixed(2) : " ") + "</div>" + "</div>"
                 inclusiveItemsDiv.appendChild(incTaxItemDiv)
             })
