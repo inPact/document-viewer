1867e2b77843139148b9ff509a3ca76d8c8e0302
gal gimpel <gal@tabit.cloud>
2018-12-27 15:36:22 +0200

    udpate

--- src/tlog-docs-template/headerService.js
+++ src/tlog-docs-template/headerService.js
@@ -225,8 +225,9 @@ export default class HeaderService {
                     var dinersTranslate = this.$translate.getText("Diners")
                     var firstName = printData.variables.F_NAME && printData.variables.F_NAME !== null ? printData.variables.F_NAME : '';
                     var lastName = printData.variables.L_NAME && printData.variables.L_NAME !== null ? printData.variables.L_NAME : '';
+                    htmlElement.classList.add('flex-center');
                     //htmlElement.innerHTML = `<span>${waiterTranslate} ${firstName} ${lastName.substring(0, 1)} - ${printData.variables.NUMBER_OF_GUESTS} ${dinersTranslate}</span>`;
-                    htmlElement.innerHTML = `<span>${waiterTranslate} ${firstName} ${lastName.substring(0, 1)} - </span><span>${printData.variables.NUMBER_OF_GUESTS} ${dinersTranslate}</span>`;
+                    htmlElement.innerHTML = `<div class="flex">${waiterTranslate} ${firstName} ${lastName.substring(0, 1)} - </div><div class="flex">${printData.variables.NUMBER_OF_GUESTS} ${dinersTranslate}</div>`;
 
                 }
                 break;
