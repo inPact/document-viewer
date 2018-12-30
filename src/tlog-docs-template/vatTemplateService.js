a112f07c6b920b5d3055e5087cb5d645d824127f
gal gimpel <gal@tabit.cloud>
2018-12-30 15:30:09 +0200

    updatwe

--- src/tlog-docs-template/vatTemplateService.js
+++ src/tlog-docs-template/vatTemplateService.js
@@ -97,7 +97,7 @@ export default class VatTemplateService {
 
             vatHeaderDiv.innerHTML = "<div class='itemDiv'>" +
                 "<div class='total-name'>" + (!(refundText === null) ? refundText : "") + (buisnessMealText ? buisnessMealText : "") + (totalAmountText ? totalAmountText : "") + "</div>" + " " +
-                "<div class='total-amount " + this.isNegative(vat.ITEM_AMOUNT) + "'>" + (this.notEmpty(vat.ITEM_AMOUNT) ? Number(vat[0].ITEM_AMOUNT).toFixed(2) : "") + "</div>" +
+                "<div class='total-amount " + this.isNegative(vat.ITEM_AMOUNT) + "'>" + (this.notEmpty(vat.ITEM_AMOUNT) ? this.twoDecimals(vat.ITEM_AMOUNT) : "") + "</div>" +
                 "</div>"
 
             vatTemplate.appendChild(vatHeaderDiv);
@@ -157,12 +157,12 @@ export default class VatTemplateService {
 
             vatTextDiv.innerHTML = "<div class='itemDiv'>" +
                 "<div class='total-name'>" + (this.notEmpty(vat.TOTAL_INCLUDED_TAX) ? vatTranslate : "") + " " + vat.VAT_PERCENT + "%" + "</div>" + " " +
-                "<div class='total-amount " + this.isNegative(vat.TOTAL_INCLUDED_TAX) + "'>" + (this.notEmpty(vat.TOTAL_INCLUDED_TAX) ? Number(vat.TOTAL_INCLUDED_TAX).toFixed(2) : "") + "</div>" +
+                "<div class='total-amount " + this.isNegative(vat.TOTAL_INCLUDED_TAX) + "'>" + (this.notEmpty(vat.TOTAL_INCLUDED_TAX) ? this.twoDecimals(vat.TOTAL_INCLUDED_TAX) : "") + "</div>" +
                 "</div>";
 
             includeVatDiv.innerHTML = "<div class='itemDiv'>" +
                 "<div class='total-name'>" + (this.notEmpty(vat.TOTAL_IN_VAT) ? includeVatTranslate : "") + "</div>" + " " +
-                "<div class='total-amount " + this.isNegative(vat.TOTAL_IN_VAT) + "'>" + (this.notEmpty(vat.TOTAL_IN_VAT) ? Number(vat.TOTAL_IN_VAT).toFixed(2) : "") + "</div>" +
+                "<div class='total-amount " + this.isNegative(vat.TOTAL_IN_VAT) + "'>" + (this.notEmpty(vat.TOTAL_IN_VAT) ? this.twoDecimals(vat.TOTAL_IN_VAT) : "") + "</div>" +
                 "</div>";
         }
 
