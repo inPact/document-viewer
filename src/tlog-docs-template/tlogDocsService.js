879807772bb6ef7391c7358f2b1cbe5d216469cc
gal gimpel <gal@tabit.cloud>
2019-01-02 17:47:13 +0200

    update

--- src/tlog-docs-template/tlogDocsService.js
+++ src/tlog-docs-template/tlogDocsService.js
@@ -326,8 +326,8 @@ export default class TlogDocsService {
         console.log("documentInfo");
         console.log(documentInfo);
         console.log("");
-        console.log("options");
-        console.log(options);
+        console.log("document");
+        console.log(document);
         console.log("");
 
         switch (_.get(document, 'printData.collections.PAYMENT_LIST[0].P_TENDER_TYPE')) {
