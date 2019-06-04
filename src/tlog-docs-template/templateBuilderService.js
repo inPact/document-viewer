4b9a274c52d0f00dcae15eb9e8c613497ac85734
gal gimpel <gal@tabit.cloud>
2019-06-04 15:05:23 +0300

    update logo

--- src/tlog-docs-template/templateBuilderService.js
+++ src/tlog-docs-template/templateBuilderService.js
@@ -358,12 +358,14 @@ export default class TemplateBuilderService {
             let elementFooterText = this.$htmlCreator.create({
                 type: 'div',
                 id: 'element-footer-text',
+                classList: [],
                 value: 'Powered by'
             });
 
             let elementFooterImage = this.$htmlCreator.create({
                 type: 'div',
                 id: 'element-footer-image',
+                classList: [],
                 attributes: [
                     { key: 'src', value: TABIT_LOGO }
                 ]
@@ -372,6 +374,7 @@ export default class TemplateBuilderService {
             let elementFooter = this.$htmlCreator.create({
                 type: 'div',
                 id: 'element-footer',
+                classList: [],
                 children: [
                     elementFooterText,
                     elementFooterImage
