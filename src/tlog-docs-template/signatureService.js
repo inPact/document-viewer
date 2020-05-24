887483d4ab466097810d829d062cdcb0927c9f82
maySchiller <may@tabit.cluod>
2020-05-24 18:40:14 +0300

    signature

--- src/tlog-docs-template/signatureService.js
+++ src/tlog-docs-template/signatureService.js
@@ -42,30 +42,46 @@ export default class SignatureService {
                 { key: "height", value: "30px" },
             ]
         })
+        if (documentInfo.md.signature.format === 'image/png') {
+            const image = new Image();
+            image.src = documentInfo.md.signature.data;
+            image.height = '70';
+            image.width = '70';
+            const imageDiv = document.createElement('div');
+            imageDiv.style.display = 'flex';
 
-        let elementSvg = this.makeSVG('svg', {
-            'id': "svg",
-            'width': "100%",
-            'height': "70",
-            'transform': "translate(0,0)",
-            'viewBox': dimension,
-            'style': "width: 100%;"
-        });
+            imageDiv.style.justifyContent = 'center';
+            imageDiv.appendChild(image);
+            contenier.appendChild(imageDiv);
+            element.appendChild(contenier);
 
-        let path = this.makeSVG('path', {
-            d: signatureData.data,
-            'stroke': "#06067f",
-            'stroke-width': "2",
-            'stroke-linecap': "butt",
-            'fill': "none",
-            'stroke-linejoin': "miter"
-        });
+        } else {
+            let elementSvg = this.makeSVG('svg', {
+                'id': "svg",
+                'width': "100%",
+                'height': "70",
+                'transform': "translate(0,0)",
+                'viewBox': dimension,
+                'style': "width: 100%;"
+            });
+    
+            let path = this.makeSVG('path', {
+                d: signatureData.data,
+                'stroke': "#06067f",
+                'stroke-width': "2",
+                'stroke-linecap': "butt",
+                'fill': "none",
+                'stroke-linejoin': "miter"
+            });
+    
+            elementSvg.appendChild(path);
+    
+            contenier.appendChild(elementSvg);
+    
+            element.appendChild(contenier);
+        }
 
-        elementSvg.appendChild(path);
-
-        contenier.appendChild(elementSvg);
-
-        element.appendChild(contenier);
+       
 
         return element;
     }
