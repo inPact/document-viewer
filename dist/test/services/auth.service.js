fcd9512078997867b5aa098334f65d91abac934d
maySchiller <may@tabit.cluod>
2020-07-20 10:51:12 +0300

    TAB-8369-timezone-support continue

--- dist/test/services/auth.service.js
+++ dist/test/services/auth.service.js
@@ -5,7 +5,6 @@ angular.module('app')
     .factory('AuthService', function ($http, ENV) {
 
         let EP = ENV.ep;
-        let timezone = null;
 
         const AUTH_URL = '/oauth2/token';
         const _authClientId = 'VbXPFm2RMiq8I2eV7MP4ZQ';
