dbd63e8b0fa0a3e67555bbb534fe25c8e85f83d8
Zohar Rosenbush <zohar.rosenbush@tabit.cloud>
2024-08-05 16:36:39 +0300

    [TAB-37449] Returned app.component and orderService to original

--- dist/test/services/order.service.js
+++ dist/test/services/order.service.js
@@ -4,24 +4,24 @@ angular.module('app')
     .constant("ENV", {
         // ep: 'https://ros-rp.tabit.cloud',
         // ep: 'https://il-int-ros.tabit-stage.com',
-        ep: 'https://us-int-ros.tabit-stage.com',
+        ep: 'https://au-int-ros.tabit-stage.com',
         // ep: 'https://au-ros.tabit-stage.com',
         // org: '64a66b305f56a25a0a4b298f',
         // tlog: '64d1047e38a00042d453ba9b',
-        org: '661e6d1c934be176e46d4459',
-        // tlog: '6644708426979e690227aad8',
+        org: '64f98aad3ed2c6bf8d0f696c',
+        tlog: '6644708426979e690227aad8',
         // tlog: '660bb4e497ac6d725ef58e08',
         // locale: 'he-IL',
         locale: 'en-US',
         // locale: 'en-AU',
         // realRegion: 'IL',
-        realRegion: 'US',
-        // realRegion: 'AU',
+        // realRegion: 'US',
+        realRegion: 'AU',
         // realRegion: 'GR',
-        // username: 'avishai@tabit.cloud',
-        // pass: 'tehilaBen100',
-        username: 'integrations-test@tabit.cloud',
-        pass: 'Tc123!@#',
+        username: 'avishai@tabit.cloud',
+        pass: 'tehilaBen100',
+        // username: 'integrations-test@tabit.cloud',
+        // pass: 'Tc123!@#',
         version: '1.34.0',
     })
     .factory('OrderService', function ($http, ENV) {
@@ -34,9 +34,6 @@ angular.module('app')
         const PRINT_DATA = 'printdata';
 
         function findOne(id, options) {
-            id = '66b0708c9e7ea46df6ca3e8d'
-            options.status = 'open'
-            console.log('zohar', options)
 
             let URL = options.status === 'closed' ? TLOGS_URL : ORDERS_URL;
 
@@ -46,8 +43,6 @@ angular.module('app')
         }
 
         function getBillPrintData(id, options) {
-            id = '66b0708c9e7ea46df6ca3e8d'
-            options.status = 'open'
 
             let URL = options.status === 'closed' ? TLOGS_URL : ORDERS_URL;
 
