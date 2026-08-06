22f35671e40e2019e68af916d199906485c20d3b
nathanp <nathan.preel@tabit.cloud>
2026-08-06 15:35:55 +0300

    ref(tax-invoice): rename printData variables 'IL_TAX_ALLOCATION_**' -> 'TAX_ALLOCATION_**'

--- src/tlog-docs-template/tlogDocsTranslate.js
+++ src/tlog-docs-template/tlogDocsTranslate.js
@@ -322,8 +322,8 @@ export default class TlogDocsTranslateService {
                 "INTERNATIONAL_ISSUER":"International Card Issuer",
                 "IN_TAKE_RECEIPT": "Receipt {{number}}",
                 "IN_TAKE_REFUND": "Receipt {{number}} Refund",
-                "IL_TAX_ALLOCATION_NUMBER": "Allocation #",
-                "IL_TAX_ALLOCATION_PENDING": "Pending Allocation",
+                "TAX_ALLOCATION_NUMBER": "Allocation #",
+                "TAX_ALLOCATION_PENDING": "Pending Allocation",
             },
             "he-IL": {
                 "POINTS_REDEMPTION": "מימוש נקודות",
@@ -609,8 +609,8 @@ export default class TlogDocsTranslateService {
                 "REWARD_AMOUNT": "שווי כרטיס מתנה",
                 "IN_TAKE_RECEIPT": "קבלה מס׳ ",
                 "IN_TAKE_REFUND": "קבלה זיכוי מס׳ ",
-                "IL_TAX_ALLOCATION_NUMBER": "מספר הקצאה",
-                "IL_TAX_ALLOCATION_PENDING": "בהמתנה למספר הקצאה",
+                "TAX_ALLOCATION_NUMBER": "מספר הקצאה",
+                "TAX_ALLOCATION_PENDING": "בהמתנה למספר הקצאה",
             }
         };
 
